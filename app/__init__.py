7a635dcb03d55f044cf8e90f2e3e064dfa64ab3c
MaryAnne45 <mary.jereh@gmail.com>
2018-04-16 01:09:06 +0300

    created the onepitch database

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
 # Creating the instances
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app(config_name):
 #creating the app instance
@@ -11,11 +13,12 @@ def create_app(config_name):
     app.config.from_object(config_options[config_name])
 #intialzing the extensions
     bootstrap.init_app(app)
+    db.init_app(app)
 #importing the instance main
     from .main import main as main_blueprint
 #then register the blueprint
     app.register_blueprint(main_blueprint)
     from .request import configure_request
     configure_request(app)
-    
+
     return app
