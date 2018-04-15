a2eccc48ab194576b36900ac915a963266ad0b05
MaryAnne45 <mary.jereh@gmail.com>
2018-04-16 01:31:28 +0300

    created a user and a pitches table

--- app/models.py
+++ app/models.py
@@ -1,5 +1,20 @@
+from . import db
+
 class Images:
     def __init__(self,id,userImageURL,webformatURL):
         self.id= id
         self.userImageURL=userImageURL
         self.webformatURL=webformatURL
+class User(db.Model):
+    __tablename__='users'
+    id=db.Column(db.Integer,primary_key = True)
+    username=db.Column(db.String(255))
+    pitch_id=db.Column(db.Integer,db.ForeignKey('pitches.id'))
+    def __repr__(self):
+        return f'User {self.username}'
+class Pitch(db.Model):
+    __tablename__='pitches'
+    id=db.Column(db.Integer,primary_key=True)
+    category=db.Column(db.Integer,primary_key=True)
+    def __repr__(self):
+        return f'User {self.username}'
--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     PIX_BASE_URL = 'https://pixabay.com/api/?key={}&category={}&image_type=photo'
     PIX_API_KEY=os.environ.get('PIX_API_KEY')
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://maryanne:mustu.cat@localhost/onepitch'
 
 class ProdConfig(Config):
     pass
